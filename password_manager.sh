2fb23ea225adda724d871ee6bb16a72a729759a0
Furuno <furunomasaharu@kinnikuMac.local>
2023-05-05 16:36:48 +0900

    コメントを削除

--- password_manager.sh
+++ password_manager.sh
@@ -51,16 +51,3 @@ while true; do
       ;;
   esac
 done
-
-
-# echo "パスワードマネージャーへようこそ"
-# echo "サービス名を入力してください"
-# read service_name
-# echo "ユーザー名を入力してください"
-# read username
-# echo "パスワードを入力してください"
-# read password
-
-# echo "サービス名:${service_name}, ユーザー名:${username}, パスワード:${password}" >> $password_file
-
-# echo "Thank you!"
