970830a86ad9945202af8014717ecb4eb52c6bd6
Furuno <furunomasaharu@kinnikuMac.local>
2023-05-05 16:19:48 +0900

    パスワードを取得する処理を追加

--- password_manager.sh
+++ password_manager.sh
@@ -1,14 +1,63 @@
 #!/bin/bash
 password_file="passwords.txt"
 
+add_password() {
+  echo "サービス名を入力してください"
+  read service_name
+  echo "ユーザー名を入力してください"
+  read username
+  echo "パスワードを入力してください"
+  read password
+
+  echo "サービス名:${service_name}, ユーザー名:${username}, パスワード:${password}" >> $password_file
+
+  echo "パスワードの追加は成功しました"
+}
+
+get_password() {
+  echo "サービス名を入力してください"
+  read service_name
+  result=$(grep -F "サービス名:${service_name}," $password_file)
+
+  if [ -n "$result" ]; then
+    echo "$result"
+  else
+    echo "そのサービス名は登録されていません。"
+  fi
+}
+
 echo "パスワードマネージャーへようこそ"
-echo "サービス名を入力してください"
-read service_name
-echo "ユーザー名を入力してください"
-read username
-echo "パスワードを入力してください"
-read password
 
-echo "サービス名:${service_name}, ユーザー名:${username}, パスワード:${password}" >> $password_file
+while true; do
+  echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+  read choice
 
-echo "Thank you!"
+  case $choice in
+    "Add Password" )
+      add_password
+      ;;
+    "Get Password" )
+      get_password
+      ;;
+    "Exit" )
+      echo "Thank you!"
+      break
+      ;;
+    * )
+      echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+      ;;
+  esac
+done
+
+
+# echo "パスワードマネージャーへようこそ"
+# echo "サービス名を入力してください"
+# read service_name
+# echo "ユーザー名を入力してください"
+# read username
+# echo "パスワードを入力してください"
+# read password
+
+# echo "サービス名:${service_name}, ユーザー名:${username}, パスワード:${password}" >> $password_file
+
+# echo "Thank you!"
